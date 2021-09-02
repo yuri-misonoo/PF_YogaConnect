aa410f8d7e92a9ff78a96f5927b0720d3a523c21
yuri-misonoo <yuri.panpanda@gmail.com>
2021-09-02 13:03:14 +0000

    [add]ルーティング

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin create!(
-  email: 'admin@admin.com'
-  password: 'test123'
+  email: 'admin@admin.com',
+  password: 'test1234'
   )
+  
