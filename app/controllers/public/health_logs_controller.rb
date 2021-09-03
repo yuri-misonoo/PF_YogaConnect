3482307f818b6a504f21fcf813d4f241dddd3154
yuri-misonoo <yuri.panpanda@gmail.com>
2021-09-03 12:30:37 +0000

    [add]publicのコントローラ、登録後の遷移先

---
+++ app/controllers/public/health_logs_controller.rb
@@ -0,0 +1,34 @@
+class Public::HealthLogsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
+  def graph  
+  end
+  
+  def memo
+  end
+
+  def calender
+  end
+  
+end
---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,22 @@
+class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
