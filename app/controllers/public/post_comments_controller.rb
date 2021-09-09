a6aeb90147e0c0aafa827d8097f1565ff34739c3
yuri-misonoo <yuri.panpanda@gmail.com>
2021-09-09 12:44:02 +0000

    [add]非同期通信

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,23 +1,25 @@
 class Public::PostCommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
-    post = Post.find(params[:post_id])
-    body = current_user.post_comments.new(post_comment_params)
-    body.post_id = post.id
-    body.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    @post_comment.save
+    render :index
   end
 
-  def destory
-    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+  def destroy
+    @post = Post.find(params[:post_id])
+    post_comment = @post.post_comments.find(params[:id])
+    post_comment.destroy
+    render :index
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:body)
   end
-  
+
 end
