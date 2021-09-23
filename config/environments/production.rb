b42a1780cab544eaf72c99a9b4f0a14abea13aeb
yuri-misonoo <yuri.panpanda@gmail.com>
2021-09-23 12:16:26 +0000

    [add]production.rbにmailerの記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  mail = ENV["SEND_MAIL"]
+  pass = ENV["SEND_MAIL_PASSWORD"]
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  port: 587,
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
+  user_name: mail,#自分のメールアドレス
+  password: pass,#アプリパスワード二段階設定をonにして、コードを取得する
+  authentication: 'plain',
+  enable_starttls_auto: true
+  }
 end
